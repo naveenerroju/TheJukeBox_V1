964016e854805e92aa81eace2b83e69f196e0760
naveen kumar <naveenkumarerroju@gmail.com>
2024-05-08 09:14:34 +0530

    debugging testcase

--- src/test/java/com/naveen/jukebox/AppTest.java
+++ src/test/java/com/naveen/jukebox/AppTest.java
@@ -13,13 +13,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("AppTest")
-class AppTest {
+public class AppTest {
     private final PrintStream standardOut = System.out;
     private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
 
 
     @BeforeEach
-    void setUp() {
+    public void setUp() {
         System.setOut(new PrintStream(outputStreamCaptor));
     }
 
@@ -28,53 +28,51 @@ class AppTest {
     void runTest1(){
 
         //Arrange
-        List<String> strings = new ArrayList<>();
-        strings.add("INPUT_FILE=jukebox-input.txt");
-        List<String> arguments= new ArrayList<>(strings);
+        List<String> arguments= new ArrayList<>(List.of("INPUT_FILE=jukebox-input.txt"));
 
-		String expectedOutput = "Songs Loaded successfully\n"+
-        "1 Kiran\n"+
-        "Playlist ID - 1\n"+
-        "Playlist ID - 2\n"+
-        "Delete Successful\n"+
-        "Current Song Playing\n"+
-        "Song - South of the Border\n"+
-        "Album - No.6 Collaborations Project\n"+
-        "Artists - Ed Sheeran,Cardi.B,Camilla Cabello\n"+
-        "Playlist ID - 1\n"+
-        "Playlist Name - MY_PLAYLIST_1\n"+
-        "Song IDs - 1 4 5 6 7\n"+
-        "Playlist ID - 1\n"+
-        "Playlist Name - MY_PLAYLIST_1\n"+
-        "Song IDs - 1 4 5 6\n"+
-        "Current Song Playing\n"+
-        "Song - Cross Me\n"+
-        "Album - No.6 Collaborations Project\n"+
-        "Artists - Ed Sheeran,Chance The Rapper,PnB Rock\n"+
-        "Current Song Playing\n"+
-        "Song - Give Life Back To Music\n"+
-        "Album - Random Access Memories\n"+
-        "Artists - Daft Punk,Nile Rodgers\n"+
-        "Current Song Playing\n"+
-        "Song - South of the Border\n"+
-        "Album - No.6 Collaborations Project\n"+
-        "Artists - Ed Sheeran,Cardi.B,Camilla Cabello\n"+
-        "Current Song Playing\n"+
-        "Song - Give Life Back To Music\n"+
-        "Album - Random Access Memories\n"+
-        "Artists - Daft Punk,Nile Rodgers\n"+
-        "Current Song Playing\n"+
-        "Song - Cross Me\n"+
-        "Album - No.6 Collaborations Project\n"+
-        "Artists - Ed Sheeran,Chance The Rapper,PnB Rock\n"+
-        "Given song id is not a part of the active playlist";
+        String expectedOutput = "Songs Loaded successfully\n"+
+                "1 Kiran\n"+
+                "Playlist ID - 1\n"+
+                "Playlist ID - 2\n"+
+                "Delete Successful\n"+
+                "Current Song Playing\n"+
+                "Song - South of the Border\n"+
+                "Album - No.6 Collaborations Project\n"+
+                "Artists - Ed Sheeran,Cardi.B,Camilla Cabello\n"+
+                "Playlist ID - 1\n"+
+                "Playlist Name - MY_PLAYLIST_1\n"+
+                "Song IDs - 1 4 5 6 7\n"+
+                "Playlist ID - 1\n"+
+                "Playlist Name - MY_PLAYLIST_1\n"+
+                "Song IDs - 1 4 5 6\n"+
+                "Current Song Playing\n"+
+                "Song - Cross Me\n"+
+                "Album - No.6 Collaborations Project\n"+
+                "Artists - Ed Sheeran,Chance The Rapper,PnB Rock\n"+
+                "Current Song Playing\n"+
+                "Song - Give Life Back To Music\n"+
+                "Album - Random Access Memories\n"+
+                "Artists - Daft Punk,Nile Rodgers\n"+
+                "Current Song Playing\n"+
+                "Song - South of the Border\n"+
+                "Album - No.6 Collaborations Project\n"+
+                "Artists - Ed Sheeran,Cardi.B,Camilla Cabello\n"+
+                "Current Song Playing\n"+
+                "Song - Give Life Back To Music\n"+
+                "Album - Random Access Memories\n"+
+                "Artists - Daft Punk,Nile Rodgers\n"+
+                "Current Song Playing\n"+
+                "Song - Cross Me\n"+
+                "Album - No.6 Collaborations Project\n"+
+                "Artists - Ed Sheeran,Chance The Rapper,PnB Rock\n"+
+                "Given song id is not a part of the active playlist";
         //Act
         App.run(arguments);
-
+        String actual = outputStreamCaptor.toString().trim();
         //Assert
-        Assertions.assertEquals(expectedOutput,outputStreamCaptor.toString().trim());
- 
-	}
+        Assertions.assertEquals(expectedOutput,actual);
+
+    }
 
     @AfterEach
     public void tearDown() {
